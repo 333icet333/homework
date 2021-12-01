6ec1e11d4e8f1edfb29b9caff5edaba6aac08849
333ice333 <05KvrasD307>
2021-12-01 20:50:59 +0200

    add test 4

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-# word = input("Введите слово: ")
-# for char in range(len(word) - 1, -1, -1):
-#   print(word[char], end="")
-# print("\n")
-
-
-
-robj = range(5)
-for it in robj:
-    print(it, end = ",")
+word = input("Введите слово: ")
+for char in range(len(word) - 1, -1, -1):
+  print(word[char], end="")
+print("\n")
